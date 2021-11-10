eeb7cc1a8f1b827f3d111b18eb091b644ff6dcbb
DiegoRG9 <94081750+DiegoRG9@users.noreply.github.com>
2021-11-10 15:38:01 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAfR53Vbbg3sHYvYEgoHoYu_Eh9I34Rh84",
+  authDomain: "pautencls02-f996e.firebaseapp.com",
+  projectId: "pautencls02-f996e",
+  storageBucket: "pautencls02-f996e.appspot.com",
+  messagingSenderId: "914638725079",
+  appId: "1:914638725079:web:f6d2f97a49ff867b43b592",
+  measurementId: "G-D8M2WS791H"
+    });
