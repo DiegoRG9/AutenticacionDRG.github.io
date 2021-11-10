324137bc45f9412841566a223f59316a06cd4271
DiegoRG9 <94081750+DiegoRG9@users.noreply.github.com>
2021-11-10 15:43:04 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyAfR53Vbbg3sHYvYEgoHoYu_Eh9I34Rh84",
-  authDomain: "pautencls02-f996e.firebaseapp.com",
-  projectId: "pautencls02-f996e",
-  storageBucket: "pautencls02-f996e.appspot.com",
-  messagingSenderId: "914638725079",
-  appId: "1:914638725079:web:f6d2f97a49ff867b43b592",
-  measurementId: "G-D8M2WS791H"
+  apiKey: "AIzaSyDQojGXo9HsSbxVTc2dGQi9rcZbJ5Lzi-A",
+  authDomain: "autenticaciondrg.firebaseapp.com",
+  projectId: "autenticaciondrg",
+  storageBucket: "autenticaciondrg.appspot.com",
+  messagingSenderId: "358558398519",
+  appId: "1:358558398519:web:27cdb22c88615e25f767b2",
+  measurementId: "G-P3MK66S9DS"
     });
